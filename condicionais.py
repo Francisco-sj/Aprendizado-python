12cbedca416fee076c8fb76dad337f7b761fdd6b
Francisco José <franciscosj22062001@gmail.com>
2026-09-01 17:45:29 -0300

    modificação em condicionais
    Atualizei o arquivo de simples para composta.

--- condicionais.py
+++ condicionais.py
@@ -1,6 +1,10 @@
 print("temperatura: ")
 temperatura = float(input())
 
-# Condição simples
+# Condição compostas
 if temperatura > 25:
     print("está quente, irei ligar a ventilação")
+elif temperatura < 15:
+    print("está frio, irei ligar o aquecimento")
+else:
+    print("está agradável, não preciso ligar nem o aquecimento nem a ventilação")
