2abb4c18d852a230306b16721e0444f8b6e520bc
Francisco José <franciscosj22062001@gmail.com>
2026-09-03 14:56:33 -0300

    Correção na estrutura de repetição
    Lógica de incremento e decremento corrigida.

--- estrutura-repetição.py
+++ estrutura-repetição.py
@@ -10,6 +10,8 @@ while temperatura < 250:
 
 # Desafio simples: Imaginando um forno de pizza, que precisa se manter a uma temperatura de 180 graus Celsius para assar a pizza corretamente. Faça um programa que pergunte ao usuário a temperatura atual do forno e, caso esteja abaixo de 180 graus, o programa irá incrementar a temperatura em 10 graus até que atinja ou ultrapasse 180 graus, informando a cada incremento a temperatura atual do forno. E se a temperatura estiver acima de 180 graus, o programa irá retirar 10 graus até que atinja 180 graus, informando a cada decremento a temperatura atual do forno.
 temperatura_forno = float(input("Digite a temperatura atual do forno: "))
+temperatura_forno = round(temperatura_forno / 10) * 10 # Arredonda para o múltiplo de 10 mais próximo, fazendo assim a lógica de incremento e decremento funcionar corretamente.
+
 if temperatura_forno < 180:
     while temperatura_forno < 180:
         print("Aquecendo o forno! Temperatura atual: ", temperatura_forno)
